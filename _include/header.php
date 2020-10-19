b57fb9eb2e6f8dec779bcf96e61dfe45f915c6b9
Sergey Sidorov <s.i.sidorovkrd@yandex.ru>
2020-10-19 16:53:10 +0300

    start commit

---
+++ _include/header.php
@@ -0,0 +1,35 @@
+<header class="page-header">
+
+	<div class="container">
+
+		<a href="/" class="logo_instead"><span class="bold">Лаборатория</span><br>«Листовая диагностика»</a>
+
+		<nav class="main_menu">
+			<ul>
+				<li><a href="#">Главная</a></li>
+				<li><a href="#">Услуги</a></li>
+				<li><a href="#">Преимущества</a></li>
+				<li>
+					<a href="#">Программы <svg class="down_arrow"><use xlink:href="#down_arrow"></use></svg></a>
+
+					<ul class="submenu_list">
+						<li><a href="#">Яблоки</a></li>
+						<li><a href="#">Виноград</a></li>
+						<li><a href="#">Клубника</a></li>
+						<li><a href="#">Пшеница</a></li>
+					</ul>
+
+				</li>
+				<li><a href="#">О нас</a></li>
+				<li><a href="#">Контакты</a></li>
+			</ul>
+		</nav>
+
+
+		<div class="burger_wrapper">
+			<div class="burger"></div>
+		</div>
+
+	</div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Листовая диагностика - консультативно-экспертный анализ растений в Краснодарском крае и Краснодаре.</title>
+
+	<link rel="stylesheet" href="/static/css/style.css?v=<?php echo time(); ?>">
+
+</head>
+<body>
+
+	<?php include ($_SERVER['DOCUMENT_ROOT'] . '/_include/header.php');?>
+
+
+	<main>
+		<!-- Первый экран -->
+		<section class="promo_screen">
+			<div class="container">
+				<div class="text_block">
+					<p class="text size_2 uppercase">Метод получения высокого урожая</p>
+					<h1>Листовая диагностика</h1>
+					<p class="text size_1 description">Вносить ли удобрения, и какие? Приведет ли внесение удобрений к росту урожая и как отразится на его качестве? <br>На все эти вопросы может ответить только Листовая диагностика.</p>
+					<a href="#" class="btn">Подробнее</a>
+				</div>
+			</div>
+		</section>
+
+
+		<section class="steps_block">
+			<div class="container">
+				<div class="steps_wrapper">
+					<div class="step_item">
+						<span class="step_num">Этап 1-й</span>
+						<p class="step_title text size_2 bold">Определение объемов</p>
+						<p class="step_descr text size_3 center">От количества собранных образцов зависит представительность результата анализа</p>
+
+						<div class="img_wrapper">
+							<svg class="step_1"><use xlink:href="#step_1"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+					<div class="step_item">
+						<span class="step_num">Этап 2-й</span>
+						<p class="step_title text size_2 bold">Отбор образцов</p>
+						<p class="step_descr text size_3 center">Надежность рекомендаций во многом зависят от правильного отбора образцов</p>
+
+						<div class="img_wrapper">
+							<svg class="step_2"><use xlink:href="#step_2"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+					<div class="step_item">
+						<span class="step_num">Этап 3-й</span>
+						<p class="step_title text size_2 bold">Подготовка пробы</p>
+						<p class="step_descr text size_3 center">Для избежания изменений в образце он должен быть доставлен в 24 часовой срок</p>
+
+						<div class="img_wrapper">
+							<svg class="step_3"><use xlink:href="#step_3"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+					<div class="step_item">
+						<span class="step_num">Этап 4-й</span>
+						<p class="step_title text size_2 bold">Анализ образца в лаборатории</p>
+						<p class="step_descr text size_3 center">Используемые нами методы диагностики, широко применяются за рубежом</p>
+
+						<div class="img_wrapper">
+							<svg class="step_4"><use xlink:href="#step_4"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+					<div class="step_item">
+						<span class="step_num">Этап 5-й</span>
+						<p class="step_title text size_2 bold">Обработка результатов</p>
+						<p class="step_descr text size_3 center">Полученные результаты подвергаются математическому и статистическому анализу</p>
+
+						<div class="img_wrapper">
+							<svg class="step_5"><use xlink:href="#step_5"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+					<div class="step_item">
+						<span class="step_num">Этап 6-й</span>
+						<p class="step_title text size_2 bold">Вынесение заключения</p>
+						<p class="step_descr text size_3 center">По завершению заказчику выдается подробный протокол с рекомендациями и результатами</p>
+
+						<div class="img_wrapper">
+							<svg class="step_6"><use xlink:href="#step_6"></use></svg>
+							<div class="circle"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section class="preferences">
+			<div class="container">
+
+				<h2><span class="bold">Проведение анализа</span> позволяет</h2>
+
+				<div class="preferences_wrapper">
+					<div class="preference_item">
+						<span class="number">1</span>
+						<p class="text size_2 bold">Своевременно</p>
+						<p class="text size_1">определить потребности растения</p>
+					</div>
+					<div class="preference_item">
+						<span class="number">2</span>
+						<p class="text size_2 bold">Скорректировать</p>
+						<p class="text size_1">дозы внесения удобрений</p>
+					</div>
+					<div class="preference_item">
+						<span class="number">3</span>
+						<p class="text size_2 bold">Повысить</p>
+						<p class="text size_1">урожайность культуры</p>
+					</div>
+					<div class="preference_item">
+						<span class="number">4</span>
+						<p class="text size_2 bold">Увеличить</p>
+						<p class="text size_1">стрессоусточивость растений</p>
+					</div>
+					<div class="preference_item">
+						<span class="number">5</span>
+						<p class="text size_2 bold">Поднять</p>
+						<p class="text size_1">качество и сроки хранения продукции</p>
+					</div>
+				</div>
+
+
+			</div>
+		</section>
+
+
+		<section class="program_block">
+			<div class="container">
+
+				<div class="text_box">
+					<h2><span class="bold">Сезонная</span> программа</h2>
+
+					<p class="text size_1">Для получения полной картины баланса питательных веществ, анализ проводится в несколько этапов.</p>
+					<p class="text size_1">Полученные результаты по валовому содержанию 12 наиболее важных для растения элементов питания сравниваются с разработанными нормами.</p>
+					<p class="text size_1">На основе этого сравнения делается экспертное заключение и решается вопрос об использовании тех или иных удобрений.</p>
+				</div>
+
+				<div class="link_block">
+					<a href="#" class="link_card">
+						<span>яблоки</span>
+						<img src="/static/imgs/program_apple.jpg" alt="">
+					</a>
+					<a href="#" class="link_card">
+						<span>виноград</span>
+						<img src="/static/imgs/program_grape.jpg" alt="">
+					</a>
+				</div>
+
+				<div class="link_block">
+					<a href="#" class="link_card">
+						<span>клубника</span>
+						<img src="/static/imgs/program_strawberry.jpg" alt="">
+					</a>
+					<a href="#" class="link_card">
+						<span>пшеница</span>
+						<img src="/static/imgs/program_wheat.jpg" alt="">
+					</a>
+				</div>
+
+				<div class="link_block">
+					<a href="#" class="link_card link_card--big">
+						<span>все программы</span>
+						<img src="/static/imgs/program_veg.jpg" alt="">
+					</a>
+				</div>
+
+			</div>
+		</section>
+
+
+		<section class="about" id="about">
+			<div class="container">
+				<span class="bgr_text">АГРО</span>
+
+				<div class="row">
+
+					<div class="left_block">
+						<h2><span class="bold">О лаборатории</span><br>«Листовая диагностика»</h2>
+						<a href="#" class="btn btn--transparent">Подробнее</a>
+					</div>
+
+					<div class="img_wrapper">
+						<img src="/static/imgs/microscope.png" alt="">
+					</div>
+
+					<div class="right_block">
+						<p class="text size_3">Консультативно-экспертная лаборатория «Листовая диагностика» более 15 лет успешно занимается вопросами минерального питания сельскохозяйственных растений.</p>
+						<p class="text size_3">Химико-аналитическая группа лаборатории успешно прошла сличительные испытания с ведущими отечественными и зарубежными лабораториями, в частности с Региональной лабораторией по сельскому хозяйству и виноградарству в г. Оз (Франция).</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section class="feedback">
+			<div class="container">
+				<h2><span class="bold">Заинтересовались?</span></h2>
+				<p class="descr">Оставьте контакты, мы с вами свяжемся</p>
+
+				<form action="" method="POST">
+					<div class="input_box">
+						<input type="text" class="input_text" name="name" placeholder="Ваше имя">
+					</div>
+					<div class="input_box">
+						<input type="text" class="input_text" name="phone" placeholder="Телефон">
+					</div>
+					<div class="input_box">
+						<input type="text" class="input_text" name="mail" placeholder="E-mail">
+					</div>
+					<div class="custom_checkbox">
+			      <input id="confirmation" type="checkbox">
+			      <label for="confirmation" name="confirmation"><span>Я согласен на <a href="#">обработку персональных данных</a></span></label>
+			    </div>
+					<input type="submit" name="" value="Отправить" class="btn">
+				</form>
+			</div>
+		</section>
+
+	</main>
+
+
+	<?php include ($_SERVER['DOCUMENT_ROOT'] . '/_include/footer.php');?>
+	<?php include ($_SERVER['DOCUMENT_ROOT'] . '/_include/svg_icons.php');?>
+
+</body>
+</html>
